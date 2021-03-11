34025d565c608aed62ab562a89d0af67599c7666
Ichika <ichiromand@yandex.ru>
2021-03-11 14:23:07 +0300

    ...

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let selectList = document.querySelector('.select__list');
-let select = document.querySelector('.select');
 let selectHead = document.querySelector('.select__head');
+let selectItem = document.querySelectorAll('.select__item');
 
-select.onclick = function (){
-    selectList.classList.add('select__open-list')
-    selectHead.classList.remove('select__head')
-    selectHead.classList.add('select__head-open')
+selectHead.onclick = function (){
+    selectList.classList.add('select__open-list');
+    selectHead.classList.remove('select__head');
+    selectHead.classList.add('select__head-open');
+}
+
+for (let item of selectItem) {
+    item.onclick = function(){
+        selectHead.textContent = item.textContent;
+        selectList.classList.remove('select__open-list');
+        selectHead.classList.add('select__head');
+        selectHead.classList.add('black');
+        selectHead.classList.remove('select__head-open');
+    }
 }
