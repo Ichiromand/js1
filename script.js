40b55c3aeb86d2cb65d17df41c50bfa5c7c938c6
Ichika <ichiromand@yandex.ru>
2021-03-12 19:31:44 +0300

    ...

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ selectHead.onclick = function (){
     selectList.classList.add('select__open-list');
     selectHead.classList.remove('select__head');
     selectHead.classList.add('select__head-open');
+
 }
 
 for (let item of selectItem) {
     item.onclick = function(){
         selectHead.textContent = item.textContent;
+        selectHead.classList.add('black');
         selectList.classList.remove('select__open-list');
         selectHead.classList.add('select__head');
-        selectHead.classList.add('black');
         selectHead.classList.remove('select__head-open');
+        if (item.textContent === 'Не задан') {
+            selectHead.classList.remove('black');
+            selectHead.textContent = 'Выбрать';
+        }
     }
 }
